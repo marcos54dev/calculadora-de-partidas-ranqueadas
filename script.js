40dafdb060793961d52d602aacd16187b7d831b3
marcos38dev <marcosoliveira38@hotmail.com>
2023-11-13 11:50:25 -0300

    Projeto desafio calculadora partidas ranqueadas

---
+++ script.js
@@ -0,0 +1,28 @@
+let vitorias = 70;
+let derrotas = 20;
+let resultado = calcularNivel(vitorias, derrotas);
+console.log(resultado);
+
+function calcularNivel(vitorias, derrotas) {
+  let saldoVitorias = vitorias - derrotas;
+  let nivel;
+
+  if (vitorias <= 10) {
+    nivel = "ferro";
+  } else if (vitorias >= 11 && vitorias <= 20) {
+    nivel = "Bonze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+    nivel = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+    nivel = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+    nivel = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
+  }
+  let mensagem = `O Herói tem de saldo ${saldoVitorias} e está no nível de ${nivel}`;
+  return mensagem;
+};
+
